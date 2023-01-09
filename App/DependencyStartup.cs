1bc7bf6dc2ad2ac1d48f052e49a37648e689616d
Daniel Cazzulino <daniel@cazzulino.com>
2023-01-09 04:21:43 -0300

    Basic behavior for app with user login, access token and email retrieval

---
+++ App/DependencyStartup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using App;
+using Devlooped;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(DependencyStartup))]
+
+public class DependencyStartup : FunctionsStartup
+{
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+        builder.ConfigurationBuilder.AddUserSecrets(ThisAssembly.Project.UserSecretsId);
+    }
+
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddHttpClient();
+
+        var storage = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") == "Development"
+            ? CloudStorageAccount.DevelopmentStorageAccount
+            : CloudStorageAccount.Parse(
+                Environment.GetEnvironmentVariable("AppStorage") ??
+                builder.GetContext().Configuration["AppStorage"] ??
+                throw new InvalidOperationException("Missing AppStorage configuration."));
+
+        builder.Services.AddSingleton(storage);
+
+        builder.Services.AddSingleton(sp => TablePartition.Create<User>(
+            sp.GetRequiredService<CloudStorageAccount>(), "Sponsor", "User", x => x.Login));
+
+        builder.Services.AddSingleton(sp => TablePartition.Create<UserEmail>(
+            sp.GetRequiredService<CloudStorageAccount>(), "Sponsor", "Email", x => x.Email));
+    }
+}
