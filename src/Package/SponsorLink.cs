a637e16176d3cd004d8b7fa0f314dd9ded1813a8
Daniel Cazzulino <daniel@cazzulino.com>
2023-01-23 02:50:23 -0300

    Add backwards-compatible overloads for fine tuning sponsorlink actions

--- src/Package/SponsorLink.cs
+++ src/Package/SponsorLink.cs
@@ -19,13 +19,22 @@ public class SponsorLink
 {
     static readonly HttpClient http = new();
     static readonly Random rnd = new();
-    
-    readonly string sponsorable;
-    readonly string product;
-    readonly DiagnosticDescriptor thanksDescriptor;
+    static readonly string[] defaultTags = new[] { "DoesNotSupportF1Help" };
 
-    readonly int pauseMin;
-    readonly int pauseMax;
+    readonly string sponsorable;
+    readonly Action<SourceProductionContext, string> notInstalled;
+    readonly Action<SourceProductionContext, string> nonSponsor;
+    readonly Action<SourceProductionContext, string> activeSponsor;
+
+    /// <summary>
+    /// Creates the sponsor link instance for the given sponsorable account, used to 
+    /// check for active installation and sponsorships for the current user (given 
+    /// their configured git email).
+    /// </summary>
+    /// <param name="sponsorable">A sponsorable account that has been properly provisioned with SponsorLink.</param>
+    /// <param name="product">The product developed by <paramref name="sponsorable"/> that is checking the sponsorship link.</param>
+    public SponsorLink(string sponsorable, string product)
+        : this(sponsorable, product, 2000, 4000) { }
 
     /// <summary>
     /// Creates the sponsor link instance for the given sponsorable account, used to 
@@ -36,26 +45,76 @@ public class SponsorLink
     /// <param name="product">The product developed by <paramref name="sponsorable"/> that is checking the sponsorship link.</param>
     /// <param name="pauseMin">Min random milliseconds to apply during build for non-sponsored users. Use 0 for no pause.</param>
     /// <param name="pauseMax">Max random milliseconds to apply during build for non-sponsored users. Use 0 for no pause.</param>
-    public SponsorLink(string sponsorable, string product, int pauseMin = 2000, int pauseMax = 4000)
+    public SponsorLink(string sponsorable, string product, int pauseMin, int pauseMax)
+        : this(sponsorable,
+              (context, path) =>
+              {
+                  context.ReportDiagnostic(Diagnostic.Create(
+                      "SL02", "SponsorLink",
+                      $"{product} uses SponsorLink to properly attribute your sponsorship with {sponsorable}. Please install the GitHub app at https://github.com/apps/sponsorlink.",
+                      DiagnosticSeverity.Warning, DiagnosticSeverity.Warning,
+                      true, 1, false,
+                      $"{product} uses SponsorLink to properly attribute your sponsorship with {sponsorable}.",
+                      $"Please install the GitHub app at https://github.com/apps/sponsorlink.",
+                      helpLink: "https://github.com/apps/sponsorlink",
+                      location: Location.Create(path, new TextSpan(0, 0), new LinePositionSpan()),
+                      customTags: defaultTags));
+                  // Add a random configurable pause in this case.
+                  Thread.Sleep(rnd.Next(pauseMin, pauseMax));
+              },
+              (context, path) =>
+              {
+                  context.ReportDiagnostic(Diagnostic.Create(
+                      "SL03", "SponsorLink",
+                      $"Please consider supporting {product} development at https://github.com/sponsors/{sponsorable}.",
+                      DiagnosticSeverity.Warning, DiagnosticSeverity.Warning,
+                      true, 1, false,
+                      $"Please consider supporting {product} development at https://github.com/sponsors/{sponsorable}.",
+                      $"SponsorLink allows {product} to attribute your sponsorship with {sponsorable}.",
+                      helpLink: $"https://github.com/sponsors/{sponsorable}",
+                      location: Location.Create(path, new TextSpan(0, 0), new LinePositionSpan()),
+                      customTags: defaultTags));
+                  // Add a random configurable pause in this case.
+                  Thread.Sleep(rnd.Next(pauseMin, pauseMax));
+
+              },
+              (context, path) =>
+              {
+                  context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor(
+                      "SL04",
+                      "SponsorLink",
+                      "Thank you for supporting {0} development with your sponsorship of {1} 💟!",
+                      "SponsorLink",
+                      DiagnosticSeverity.Info,
+                      true,
+                      $"{product} uses SponsorLink to attribute your support via GitHub Sponsors.",
+                      $"https://github.com/sponsors/{sponsorable}",
+                      defaultTags),
+                      null,
+                      //location: location,
+                      product, sponsorable));
+              })
+    { }
+
+    /// <summary>
+    /// Advanced overload that allows granular behavior customization for the sponsorable account.
+    /// </summary>
+    /// <param name="sponsorable">A sponsorable account that has been properly provisioned with SponsorLink.</param>
+    /// <param name="notInstalled">Action to invoke when the user has not installed the SponsorLink app yet (or has disabled it).</param>
+    /// <param name="nonSponsor">Action to invoke when the user has installed the app but is not sponsoring.</param>
+    /// <param name="activeSponsor">Action to invoke when the user has installed the app and is sponsoring the account.</param>
+    /// <remarks>
+    /// The action delegates receive the generator context and the current project path.
+    /// </remarks>
+    public SponsorLink(string sponsorable, 
+        Action<SourceProductionContext, string> notInstalled, 
+        Action<SourceProductionContext, string> nonSponsor, 
+        Action<SourceProductionContext, string> activeSponsor)
     {
-        (this.sponsorable, this.product, this.pauseMin, this.pauseMax) = 
-            (sponsorable, product, pauseMin, pauseMax);
-
-        thanksDescriptor = new DiagnosticDescriptor(
-            "SL01",
-            "SponsorLink",
-            "Thank you for supporting {0} development with your sponsorship of {1} 💟!",
-            "SponsorLink",
-            DiagnosticSeverity.Info,
-            true,
-            $"{product} uses SponsorLink to attribute your support via GitHub Sponsors.",
-            $"https://github.com/sponsors/{sponsorable}",
-            "DoesNotSupportF1Help");
-
-        //SupportedDiagnostics = ImmutableArray.Create(thanksDescriptor,
-        //    // If we don't have at least one warning, we're not invoked at all :(
-        //    new DiagnosticDescriptor(
-        //        "SL00", "Dummy", "Dummy", "Dummy", DiagnosticSeverity.Warning, true));
+        this.sponsorable = sponsorable;
+        this.notInstalled = notInstalled;
+        this.nonSponsor = nonSponsor;
+        this.activeSponsor = activeSponsor;
     }
 
     /// <summary>
@@ -97,23 +156,20 @@ public class SponsorLink
                 Debugger.Break();
             else
                 Debugger.Launch();
-        
-        var tags = new[] { "DoesNotSupportF1Help" };
-        var location = Location.Create(states[0].Path, new TextSpan(0, 0), new LinePositionSpan());
 
         if (states.IsDefaultOrEmpty || states[0].InsideEditor == null)
         {
             // Broken state
             context.ReportDiagnostic(Diagnostic.Create(
-                "SL02", "SponsorLink",
+                "SL01", "SponsorLink",
                 "Invalid SponsorLink configuration",
                 DiagnosticSeverity.Error, DiagnosticSeverity.Error,
                 true, 0, false,
                 "Invalid SponsorLink configuration",
-                "SponsorLink has been incorrectly configured. See https://devlooped.com/sponsorlink/SL02.html.",
-                location: location,
-                helpLink: "https://devlooped.com/sponsorlink/SL02.html", 
-                customTags: tags));
+                "SponsorLink has been incorrectly configured. See https://devlooped.com/sponsorlink/SL01.html.",
+                location: Location.Create(states[0].Path, new TextSpan(0, 0), new LinePositionSpan()),
+                helpLink: "https://devlooped.com/sponsorlink/SL02.html",
+                customTags: defaultTags));
 
             return;
         }
@@ -131,60 +187,24 @@ public class SponsorLink
             return;
 
         var email = GetEmail(Path.GetDirectoryName(states[0].Path));
+        // No email configured in git. Weird.
         if (string.IsNullOrEmpty(email))
-        {
-            // No email configured in git. Weird.
             return;
-        }
 
         // Check app install and sponsoring status
         var installed = UrlExists($"https://devlooped.blob.core.windows.net/sponsorlink/apps/{email}", context.CancellationToken);
         var sponsoring = UrlExists($"https://devlooped.blob.core.windows.net/sponsorlink/devlooped/{email}", context.CancellationToken);
 
+        // Faulted HTTP HEAD request checking for url?
         if (installed == null || sponsoring == null)
-        {
-            // Faulted HTTP HEAD request checking for url?
             return;
-        }
 
         if (installed == false)
-        {
-            context.ReportDiagnostic(Diagnostic.Create(
-                "SL03", "SponsorLink",
-                $"{product} uses SponsorLink to properly attribute your sponsorship with {sponsorable}. Please install the GitHub app at https://github.com/apps/sponsorlink.",
-                DiagnosticSeverity.Warning, DiagnosticSeverity.Warning,
-                true, 1, false,
-                $"{product} uses SponsorLink to properly attribute your sponsorship with {sponsorable}.",
-                $"Please install the GitHub app at https://github.com/apps/sponsorlink.",
-                helpLink: "https://github.com/apps/sponsorlink",
-                location: location,
-                customTags: tags));
-
-            Thread.Sleep(rnd.Next(pauseMin, pauseMax));
-        }
+            notInstalled(context, states[0].Path);
         else if (sponsoring == false)
-        {
-            context.ReportDiagnostic(Diagnostic.Create(
-                "SL04", "SponsorLink",
-                $"Please consider supporting {product} development at https://github.com/sponsors/{sponsorable}.",
-                DiagnosticSeverity.Warning, DiagnosticSeverity.Warning,
-                true, 1, false,
-                $"Please consider supporting {product} development at https://github.com/sponsors/{sponsorable}.",
-                $"SponsorLink allows {product} to attribute your sponsorship with {sponsorable}.",
-                helpLink: $"https://github.com/sponsors/{sponsorable}",
-                location: location,
-                customTags: tags));
-
-            Thread.Sleep(rnd.Next(pauseMin, pauseMax));
-        }
+            nonSponsor(context, states[0].Path);
         else
-        {
-            context.ReportDiagnostic(Diagnostic.Create(
-                thanksDescriptor,
-                null,
-                //location: location,
-                product, sponsorable));
-        }
+            activeSponsor(context, states[0].Path);
     }
 
     static string? GetEmail(string workingDirectory)
