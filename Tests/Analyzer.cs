6f6705a6d2bbedf9b91b4fb2c491001af67adc1c
Daniel Cazzulino <daniel@cazzulino.com>
2023-01-23 01:39:08 -0300

    Add sanity check to ensure things work with analyzer
    At least that we can instantiate the thing after obfuscation.

---
+++ Tests/Analyzer.cs
@@ -0,0 +1,18 @@
+﻿extern alias Analyzer;
+
+using Microsoft.CodeAnalysis;
+
+namespace Devlooped;
+
+public class AnalyzerTests
+{
+    [Trait("SponsorLink", "true")]
+    [Fact]
+    public void CreateSponsorLink()
+    {
+        var link = new Analyzer::Devlooped.SponsorLink("foo", "bar");
+
+        // Due to improper init on the context, this should fail with an NRE
+        Assert.Throws<NullReferenceException>(() => link.Initialize(new IncrementalGeneratorInitializationContext()));
+    }
+}
