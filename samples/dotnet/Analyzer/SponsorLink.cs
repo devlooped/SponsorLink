9738cd57f08d2652bfa6636a8ea858c0cb4c5894
Daniel Cazzulino <daniel@cazzulino.com>
2023-01-27 20:45:30 -0300

    Updated sample to main build

---
+++ samples/dotnet/Analyzer/SponsorLink.cs
@@ -0,0 +1,14 @@
+﻿using Devlooped;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SponsorableLib;
+
+[Generator]
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base("kzu", "SponsorableLib") 
+        // NOTE: diagnostics prefix will default to K(zu)S(Sponsorable)L(Lib) > DSLxx
+    { }
+}
---
+++ samples/dotnet/Tests/Sample.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Tests;
+
+public class Sample
+{
+    [Fact]
+    public void Test()
+    {
+
+    }
+}
