d25d2db6a7dda7ad3cdb863a31467dfb81e465b2
Daniel Cazzulino <daniel@cazzulino.com>
2023-01-28 02:51:31 -0300

    Add example of how to change the diagnostic description
    We now have two examples SxS, showing the straightforward way and the advanced way.

--- samples/dotnet/Analyzer/SponsorLink.cs
+++ samples/dotnet/Analyzer/SponsorLink.cs
@@ -1,4 +1,6 @@
-﻿using Devlooped;
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Devlooped;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 
@@ -6,9 +8,35 @@ namespace SponsorableLib;
 
 [Generator]
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
-class SponsorLinker : SponsorLink
+class SimpleSponsorLinker : SponsorLink
 {
-    public SponsorLinker() : base("kzu", "SponsorableLib") 
-        // NOTE: diagnostics prefix will default to K(zu)S(Sponsorable)L(Lib) > DSLxx
+    public SimpleSponsorLinker() : base("kzu", "SponsorableLib")
+    // NOTE: diagnostics prefix will default to K(zu)S(Sponsorable)L(Lib) > DSLxx
     { }
+}
+
+
+[Generator]
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+class AdvancedSponsorLinker : SponsorLink
+{
+    static readonly SponsorLinkSettings settings;
+    
+    static AdvancedSponsorLinker()
+    {
+        // NOTE: diagnostics prefix will default to K(zu)S(Sponsorable)L(Lib) > DSLxx
+        settings = SponsorLinkSettings.Create("kzu", "AdvancedSponsorableLib");
+        // Here we showcase how to modify the built-in diagnostics to add a custom description.
+        settings.SupportedDiagnostics = settings.SupportedDiagnostics
+            .Select(x => x.IsKind(DiagnosticKind.UserNotSponsoring) ?
+                x.With(description: "Your sponsorship is used to further develop SponsorLink and make it great for the entire oss community!") :
+                x)
+            .ToImmutableArray();
+    }
+
+    public AdvancedSponsorLinker() : base(settings) { }
+
+    // Do something different on diagnostic instead of reporting? Can be anything.
+    protected override Diagnostic? OnDiagnostic(string projectPath, DiagnosticKind kind) 
+        => base.OnDiagnostic(projectPath, kind);
 }
