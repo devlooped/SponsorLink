c529f0bbc01e8f17c4204bbad9d67e6c35b9bd6a
Daniel Cazzulino <daniel@cazzulino.com>
2023-08-25 16:10:20 -0300

    Delete entire analyzer/package project which is no longer needed
    Now that the gh-sponsors CLI provides a standards-based mechanism to check for sponsorships (a JWT manifest that can be read using standard libraries), we don't need any of the existing code.
    
    This makes for a far lower barrier of adoption, removes the need for SL to make decisions for integrators and shifts the discussion to one about the value of knowing who is sponsoring a project for the purpose of feature-toggling functionality.

--- src/Tests/Analyzer.cs
+++ src/Tests/Analyzer.cs
@@ -1,5 +1,5 @@
-﻿extern alias Analyzer;
-
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Testing;
@@ -7,18 +7,37 @@ using Microsoft.CodeAnalysis.Testing.Verifiers;
 
 namespace Devlooped;
 
-[Generator]
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
-public class TestSponsorLink : Analyzer::Devlooped.SponsorLink
+public class TestSponsorLink : DiagnosticAnalyzer
 {
-    public TestSponsorLink() : base(Analyzer::Devlooped.SponsorLinkSettings.Create("testing", "foo"))
+    static DiagnosticDescriptor descriptor = new("IDE001", "IDE", "IDE", "Design", DiagnosticSeverity.Warning, true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(descriptor);
+
+    public override void Initialize(AnalysisContext context)
     {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+        if (IsEditor)
+        {
+            context.RegisterCompilationAction(c => c.ReportDiagnostic(Diagnostic.Create(descriptor, Location.None)));
+        }
     }
+
+    public static bool IsEditor => IsVisualStudio || IsRider;
+
+    public static bool IsVisualStudio =>
+        Environment.GetEnvironmentVariable("ServiceHubLogSessionKey") != null ||
+        Environment.GetEnvironmentVariable("VSAPPIDNAME") != null;
+
+    public static bool IsRider =>
+        Environment.GetEnvironmentVariable("RESHARPER_FUS_SESSION") != null ||
+        Environment.GetEnvironmentVariable("IDEA_INITIAL_DIRECTORY") != null;
 }
 
-public class AnalyzerTests
+public class AnalyzerTests(ITestOutputHelper Output)
 {
-    [Trait("SponsorLink", "true")]
     [Fact]
     public async Task CreateSponsorLinkAsync()
     {
@@ -26,14 +45,51 @@ public class AnalyzerTests
 
         test.TestCode = "// ";
 
-        // We should get the error about missing MSBuildProjectFullPath if 
-        // running the test from the IDE, but not from CLI
-        if (Environment.GetEnvironmentVariable("ServiceHubLogSessionKey") != null ||
-            Environment.GetEnvironmentVariable("VSAPPIDNAME") != null)
+        if (TestSponsorLink.IsEditor)
         {
-            test.ExpectedDiagnostics.Add(DiagnosticResult.CompilerError("DSL001"));
+            Output.WriteLine("Running inside editor");
+            test.ExpectedDiagnostics.Add(DiagnosticResult.CompilerWarning("IDE001"));
+        }
+        else
+        {
+            Output.WriteLine("Running outside editor");
         }
 
         await test.RunAsync();
+    }
+
+    // Showcases how to read the user's email using LibGit2Sharp
+    [Fact]
+    public void LibGitConfig()
+    {
+        var config = LibGit2Sharp.Configuration.BuildFrom(Directory.GetCurrentDirectory());
+        var email = config.Get<string>("user.email").Value;
+
+        Assert.Contains("@", email);
+        Output.WriteLine(email);
+    }
+
+    // Showcases how to read the user's email using an external process
+    [Fact]
+    public void GitConfig()
+    {
+        var proc = Process.Start(new ProcessStartInfo("git", "config --get user.email")
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            WorkingDirectory = Directory.GetCurrentDirectory()
+        });
+
+        proc!.WaitForExit();
+
+        Assert.Equal(0, proc.ExitCode);
+
+        if (proc.ExitCode == 0)
+        {
+            var email = proc.StandardOutput.ReadToEnd().Trim();
+            Assert.Contains("@", email);
+            Output.WriteLine(email);
+        }
     }
 }
