03bd3ff2e72e01b2fafad97fe7fc308b2bad3703
Daniel Cazzulino <daniel@cazzulino.com>
2024-02-21 17:30:48 -0300

    Add support for fetching SL manifest and signing JWT

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,10 +1,18 @@
+using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using Devlooped.SponsorLink;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        builder.AddUserSecrets("A85AC898-E41C-4D9D-AD9B-52ED748D9901");
+    })
     .ConfigureFunctionsWebApplication(builder =>
     {
         builder.UseMiddleware<ErrorLoggingMiddleware>();
@@ -15,7 +23,42 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddHttpClient();
+        services.AddHttpClient("github", http =>
+        {
+            http.BaseAddress = new Uri("https://api.github.com");
+            http.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Devlooped.SponsorLink", ThisAssembly.Info.InformationalVersion));
+        });
+        services.AddHttpClient("sponsorable", (sp, http) =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            if (config["GH_TOKEN"] is not { Length: > 0 } ghtoken)
+            {
+                sp.GetRequiredService<ILogger<Sync>>().LogWarning("Missing required configuration GH_TOKEN");
+                throw new InvalidOperationException("Missing required configuration GH_TOKEN");
+            }
+
+            http.BaseAddress = new Uri("https://api.github.com");
+            http.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Devlooped.SponsorLink", ThisAssembly.Info.InformationalVersion));
+            http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ghtoken);
+        });
+        services.AddMemoryCache();
+        services.AddSingleton(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            if (config["SPONSORLINK_KEY"] is not { Length: > 0 } key)
+            {
+                sp.GetRequiredService<ILogger<Sync>>().LogError("Missing required configration SPONSORLINK_KEY");
+                throw new InvalidOperationException("Missing required configuration SPONSORLINK_KEY");
+            }
+
+            // The key (as well as the yaml manifest) can be generated using gh sponsors init
+            // Install with: gh extension install devlooped/gh-sponsors
+            // See: https://github.com/devlooped/gh-sponsors
+            var rsa = RSA.Create();
+            rsa.ImportRSAPrivateKey(Convert.FromBase64String(key), out _);
+
+            return rsa;
+        });
     })
     .Build();
 
--- src/Web/Sync.cs
+++ src/Web/Sync.cs
@@ -1,22 +1,52 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Json;
 using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using SharpYaml.Serialization;
 
 namespace Devlooped.SponsorLink;
+
 
 /// <summary>
 /// Returns a JWT or JSON manifest of the authenticated user's claims.
 /// </summary>
-public class Sync(IConfiguration configuration)
+public class Sync(
+    ILogger<Sync> logger, IConfiguration configuration, 
+    IHttpClientFactory httpFactory, IMemoryCache cache,
+    RSA rsa)
 {
+    static readonly Serializer serializer = new(new SerializerSettings
+    {
+        NamingConvention = new CamelCaseNamingConvention()
+    });
+
+    class Manifest
+    {
+        public required string Issuer { get; set; }
+        public required string Audience { get; set; }
+        [YamlMember("pub")]
+        public required string PublicKey { get; set; }
+    }
+
+    //public IActionResult Refresh([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+    //{
+
+    //}
+
     /// <summary>
     /// Depending on the Accept header, returns a JWT or JSON manifest of the authenticated user's claims.
     /// </summary>
     [Function("sync")]
-    public IActionResult RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req, FunctionContext context)
+    public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req, FunctionContext context)
     {
         var feature = context.Features.Get<ClaimsFeature>();
         var principal = feature?.Principal ?? req.HttpContext.User;
@@ -36,19 +66,93 @@ public class Sync(IConfiguration configuration)
             return new UnauthorizedResult();
         }
 
+        if (!cache.TryGetValue<Manifest>(typeof(Manifest), out var manifest) || manifest is null)
+        {
+            // Populate manifest
+            if (configuration["SPONSORLINK_ACCOUNT"] is not { Length: > 0 } account)
+            {
+                // Auto-discovery by fetching from [user]/.github/sponsorlink.yml
+                using var gh = httpFactory.CreateClient("sponsorable");
+                var data = await gh.PostAsJsonAsync("graphql", new
+                {
+                    query = "query { viewer { login } }"
+                });
+
+                if (!data.IsSuccessStatusCode)
+                {
+                    logger.LogError("Failed to fetch sponsorable user: {StatusCode} {Reason}", data.StatusCode, await data.Content.ReadAsStringAsync());
+                    return new StatusCodeResult(500);
+                }
+
+                var viewer = await data.Content.ReadFromJsonAsync<JsonElement>();
+                account = viewer.GetProperty("data").GetProperty("viewer").GetProperty("login").GetString();
+            }
+
+            var url = $"https://github.com/{account}/.github/raw/main/sponsorlink.yml";
+
+            //NOTE: it should be a public URL
+            using var http = httpFactory.CreateClient();
+            var response = await http.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                logger.LogError("Failed to retrieve manifest from {Url}: {StatusCode} {Reason}", url, (int)response.StatusCode, await response.Content.ReadAsStringAsync());
+                return new StatusCodeResult(500);
+            }
+
+            var yaml = await response.Content.ReadAsStringAsync();
+            manifest = serializer.Deserialize<Manifest>(yaml);
+
+            if (manifest is null)
+            {
+                logger.LogError("Failed to deserialize YAML manifest from {Url}", url);
+                return new StatusCodeResult(500);
+            }
+
+            if (manifest.Audience == null)
+            {
+                // Audience defaults to the manifest url user/org
+                manifest.Audience = new Uri(url).Segments[1].Trim('/');
+            }
+
+            manifest = cache.Set(typeof(Manifest), manifest, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1)
+            });
+        }
+
+        var claims = principal.Claims;
+        // Subject will be the authenticated user id: https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.2
+        if (principal.FindFirstValue("urn:github:id") is not { Length: > 0 } subject)
+        {
+            logger.LogError("Missing required claim 'urn:github:id'");
+            return new StatusCodeResult(500);
+        }
+
+        // Prepend the shorthand sub claim
+        claims = new[] { new Claim("sub", subject) }.Concat(claims);
+
         // We always respond authenticated requests either with a JWT or JSON, depending on the Accept header.
         if (req.Headers.Accept.Contains("application/jwt"))
         {
-            var token = new JwtSecurityToken(
-                // TODO?
-                // audience: audience,
-                // issuer: issuer,
-                // signingCredentials: private key...
-                claims: principal.Claims
-            );
+            var signing = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
+            // Expire the first day of the next month
+            var expiration = DateTime.UtcNow.AddMonths(1);
+            // Use current time so they don't expire all at the same time
+            expiration = new DateTime(expiration.Year, expiration.Month, 1, 
+                DateTime.UtcNow.Hour, 
+                DateTime.UtcNow.Minute, 
+                DateTime.UtcNow.Second, 
+                DateTime.UtcNow.Millisecond, 
+                DateTimeKind.Utc);
 
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-            
+            var jwt = new JwtSecurityTokenHandler().WriteToken(new JwtSecurityToken(
+                issuer: manifest.Issuer,
+                audience: manifest.Audience,
+                signingCredentials: signing,
+                // Claims on whether he's a sponsor or not, tier, etc.
+                claims: principal.Claims
+            ));
+
             return new ContentResult
             {
                 Content = jwt,
@@ -60,7 +164,9 @@ public class Sync(IConfiguration configuration)
         {
             return new JsonResult(new
             {
-                claims = principal.Claims.ToDictionary(x => x.Type, x => x.Value),
+                issuer = manifest.Issuer,
+                audience = manifest.Audience,
+                claims = claims.ToDictionary(x => x.Type, x => x.Value),
 #if DEBUG
                 headers = req.Headers.ToDictionary(x => x.Key, x => x.Value.ToString().Trim('"'))
 #endif
