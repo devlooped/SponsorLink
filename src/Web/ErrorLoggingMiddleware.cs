2b0451b13f32a154f2eded768e20360303974d61
Daniel Cazzulino <daniel@cazzulino.com>
2024-02-21 12:26:26 -0300

    Add initial boilerplate sync function app
    Based on the boiler sample from https://github.com/kzu/Auth0Functions

---
+++ src/Web/ErrorLoggingMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Logging;
+
+namespace Devlooped.SponsorLink;
+
+public class ErrorLoggingMiddleware : IFunctionsWorkerMiddleware
+{
+    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            var logger = context.GetLogger<ErrorLoggingMiddleware>();
+            logger.LogError(e, "Exception: {Exception}", e.ToString());
+            throw;
+        }
+    }
+}
