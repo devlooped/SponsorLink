bedc05c84543d65135029d4fcf3bf171dcb31c4e
Daniel Cazzulino <daniel@cazzulino.com>
2024-04-09 16:39:29 -0300

    Allow server-side logging of config values
    Whether for development or DEBUG env

--- src/Web/Version.cs
+++ src/Web/Version.cs
@@ -1,24 +1,36 @@
 ﻿using System.Reflection;
 using System.Security.Cryptography;
 using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Devlooped.Sponsors;
 
-class Version(IConfiguration configuration)
+class Version(IConfiguration configuration, ILogger<Version> logger, IWebHostEnvironment hosting)
 {
     [Function("version")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
-        => new ContentResult
+    {
+        if (hosting.EnvironmentName == "Development" || configuration["DEBUG"] == "true")
+        {
+            foreach (var item in configuration.AsEnumerable())
+            {
+                logger.LogInformation("{Key} = {Value}", item.Key, item.Value);
+            }
+        }
+
+        return new ContentResult
         {
             Content = Assembly.GetExecutingAssembly().GetName().Version?.ToString(3),
             ContentType = "text/plain",
             StatusCode = 200,
         };
+    }
 
     [Function("pub")]
     public IActionResult Test([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
