39fa4f2ed12d4f9dcf0a093d9e5bbde721cee53c
Daniel Cazzulino <daniel@cazzulino.com>
2024-05-18 16:00:54 -0300

    Add standalone offline manifest validation helper
    This should be easily reusable from MSBuild tasks, analyzers and other tools alike.

---
+++ src/Core/Manifest.cs
@@ -0,0 +1,100 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Devlooped.Sponsors;
+
+public enum ManifestStatus
+{
+    /// <summary>
+    /// The manifest couldn't be read at all.
+    /// </summary>
+    Unknown,
+    /// <summary>
+    /// The manifest was read and is valid (not expired and properly signed).
+    /// </summary>
+    Valid,
+    /// <summary>
+    /// The manifest was read but has expired.
+    /// </summary>
+    Expired,
+    /// <summary>
+    /// 
+    /// </summary>
+    Invalid,
+}
+
+/// <summary>
+/// Validates manifests in JWT format.
+/// </summary>
+public static class Manifest
+{
+    /// <summary>
+    /// Validates the manifest signature and optional expiration.
+    /// </summary>
+    /// <param name="jwt">The JWT to validate.</param>
+    /// <param name="key">The key to validate the manifest signature with.</param>
+    /// <param name="token">Except when returning <see cref="ManifestStatus.Unknown"/>, returns the security token read from the JWT, even if signature check failed.</param>
+    /// <param name="principal">The associated claims, only when return value is not <see cref="ManifestStatus.Unknown"/>.</param>
+    /// <param name="requireExpiration">Whether to check for expiration.</param>
+    /// <returns>The status of the validation.</returns>
+    public static ManifestStatus Validate(string jwt, RSA key, out SecurityToken? token, out ClaimsPrincipal? principal, bool validateExpiration)
+        => Validate(jwt, new RsaSecurityKey(key), out token, out principal, validateExpiration);
+
+    /// <summary>
+    /// Validates the manifest signature and optional expiration.
+    /// </summary>
+    /// <param name="jwt">The JWT to validate.</param>
+    /// <param name="key">The key to validate the manifest signature with.</param>
+    /// <param name="token">Except when returning <see cref="ManifestStatus.Unknown"/>, returns the security token read from the JWT, even if signature check failed.</param>
+    /// <param name="principal">The associated claims, only when return value is not <see cref="ManifestStatus.Unknown"/>.</param>
+    /// <param name="requireExpiration">Whether to check for expiration.</param>
+    /// <returns>The status of the validation.</returns>
+    public static ManifestStatus Validate(string jwt, SecurityKey key, out SecurityToken? token, out ClaimsPrincipal? principal, bool validateExpiration)
+    {
+        token = default;
+        principal = default;
+        var handler = new JwtSecurityTokenHandler();
+
+        if (!handler.CanReadToken(jwt))
+            return ManifestStatus.Unknown;
+
+        var validation = new TokenValidationParameters
+        {
+            RequireExpirationTime = false,
+            ValidateLifetime = false,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key,
+        };
+
+        try
+        {
+            principal = handler.ValidateToken(jwt, validation, out token);
+            if (validateExpiration && token.ValidTo == DateTime.MinValue)
+                return ManifestStatus.Invalid;
+
+            // The sponsorable manifest does not have an expiration time.
+            if (validateExpiration && token.ValidTo < DateTimeOffset.UtcNow)
+                return ManifestStatus.Expired;
+
+            return ManifestStatus.Valid;
+        }
+        catch (SecurityTokenInvalidSignatureException)
+        {
+            var jwtToken = handler.ReadJwtToken(jwt);
+            token = jwtToken;
+            principal = new ClaimsPrincipal(new ClaimsIdentity(jwtToken.Claims));
+            return ManifestStatus.Invalid;
+        }
+        catch (SecurityTokenException)
+        {
+            var jwtToken = handler.ReadJwtToken(jwt);
+            token = jwtToken;
+            principal = new ClaimsPrincipal(new ClaimsIdentity(jwtToken.Claims));
+            return ManifestStatus.Invalid;
+        }
+    }
+}
---
+++ src/Tests/ManifestTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Devlooped.Sponsors;
+
+namespace Devlooped.Tests;
+
+public class ManifestTests
+{
+    [Fact]
+    public void ValidateSponsorable()
+    {
+        var manifest = SponsorableManifest.Create(new Uri("https://foo.com"), [new Uri("https://github.com/sponsors/bar")], "ASDF1234");
+        var jwt = manifest.ToJwt();
+
+        // NOTE: sponsorable manifest doesn't have expiration date.
+        var status = Manifest.Validate(jwt, manifest.SecurityKey, out var token, out var principal, false);
+
+        Assert.Equal(ManifestStatus.Valid, status);
+    }
+
+    [Fact]
+    public void ValidateWrongKey()
+    {
+        var manifest = SponsorableManifest.Create(new Uri("https://foo.com"), [new Uri("https://github.com/sponsors/bar")], "ASDF1234");
+        var jwt = manifest.ToJwt();
+
+        var key = RSA.Create();
+
+        var status = Manifest.Validate(jwt, key, out var token, out var principal, false);
+
+        Assert.Equal(ManifestStatus.Invalid, status);
+
+        // We should still be a able to read the data, knowing it may have been tampered with.
+        Assert.NotNull(principal);
+        Assert.NotNull(token);
+    }
+
+    [Fact]
+    public void ValidateExpiredSponsor()
+    {
+        var manifest = SponsorableManifest.Create(new Uri("https://foo.com"), [new Uri("https://github.com/sponsors/bar")], "ASDF1234");
+        var sponsor = manifest.Sign([], expiration: TimeSpan.Zero);
+
+        // Will be expired after this.
+        Thread.Sleep(1000);
+
+        var status = Manifest.Validate(sponsor, manifest.SecurityKey, out var token, out var principal, true);
+
+        Assert.Equal(ManifestStatus.Expired, status);
+
+        // We should still be a able to read the data, even if expired (but not tampered with).
+        Assert.NotNull(principal);
+        Assert.NotNull(token);
+    }
+
+    [Fact]
+    public void ValidateUnknownFormat()
+    {
+        var manifest = SponsorableManifest.Create(new Uri("https://foo.com"), [new Uri("https://github.com/sponsors/bar")], "ASDF1234");
+
+        var status = Manifest.Validate("asdfasdf", manifest.SecurityKey, out var token, out var principal, false);
+
+        Assert.Equal(ManifestStatus.Unknown, status);
+
+        // Nothing could be read at all.
+        Assert.Null(principal);
+        Assert.Null(token);
+    }
+}
