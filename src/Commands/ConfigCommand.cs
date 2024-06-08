899512191c193f29bd4622d8693a3a7f17474c6e
Daniel Cazzulino <daniel@cazzulino.com>
2024-06-08 03:14:31 -0300

    Delete credentials when removing manifests, add global clear
    The new `config --clear` will remove all cached creds. Should make it easier to reset the creds used for each sponsorable account (interactive login will be needed for each again to sync/refresh).
    
    Improved rendering of messages when removing manifests, since we now also delete creds so we don't leave those behind.

--- src/Commands/ConfigCommand.cs
+++ src/Commands/ConfigCommand.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel;
 using DotNetConfig;
+using Spectre.Console;
 using Spectre.Console.Cli;
 using static Spectre.Console.AnsiConsole;
-using static ThisAssembly.Strings;
+using static ThisAssembly;
 
 namespace Devlooped.Sponsors;
 
@@ -14,6 +15,16 @@ public class ConfigCommand(Config config) : Command<ConfigCommand.ConfigSettings
         [Description("Enable or disable automatic synchronization of expired manifests.")]
         [CommandOption("--autosync")]
         public bool? AutoSync { get; set; }
+
+        [Description("Clear all cached credentials used for manifest synchronization.")]
+        [CommandOption("--clear")]
+        public bool? Clear { get; set; }
+
+        /// <summary>
+        /// Property used to modify the namespace from tests for scoping stored passwords.
+        /// </summary>
+        [CommandOption("--namespace", IsHidden = true)]
+        public string Namespace { get; set; } = "com.devlooped";
     }
 
     public override int Execute(CommandContext context, ConfigSettings settings)
@@ -23,9 +34,22 @@ public class ConfigCommand(Config config) : Command<ConfigCommand.ConfigSettings
             // We persist as a string since that makes it easier to parse from MSBuild.
             config.SetString("sponsorlink", "autosync", settings.AutoSync.Value.ToString().ToLowerInvariant());
             if (settings.AutoSync == true)
-                MarkupLine(Sync.AutoSyncEnabled);
+                MarkupLine(Strings.Sync.AutoSyncEnabled);
             else
-                MarkupLine(Sync.AutoSyncDisabled);
+                MarkupLine(Strings.Sync.AutoSyncDisabled);
+        }
+
+        if (settings.Clear == true)
+        {
+            // Check existing creds, if any
+            var store = GitCredentialManager.CredentialManager.Create(settings.Namespace);
+            var accounts = store.GetAccounts("https://github.com");
+            foreach (var clientId in accounts)
+                store.Remove("https://github.com", clientId);
+
+            MarkupLine(Strings.Config.Clear(accounts.Count));
+            foreach (var clientId in accounts)
+                Write(new Padder(new Markup(Strings.Config.ClearClientId(clientId)), new Padding(3, 0, 0, 0)));
         }
 
         return 0;
