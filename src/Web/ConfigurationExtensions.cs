cfaf17b26946b7f213fc526125d71da9741af30d
Daniel Cazzulino <daniel@cazzulino.com>
2024-08-21 23:24:24 -0300

    Ensure envvars are configured always, before keyvault

--- src/Web/ConfigurationExtensions.cs
+++ src/Web/ConfigurationExtensions.cs
@@ -8,6 +8,8 @@ public static class ConfigurationExtensions
     public static IConfigurationBuilder Configure(this IConfigurationBuilder builder)
     {
         builder.AddUserSecrets("A85AC898-E41C-4D9D-AD9B-52ED748D9901");
+        builder.AddEnvironmentVariables();
+
         // Optionally, use key vault for secrets instead of plain-text app service configuration
         if (Environment.GetEnvironmentVariable("AZURE_KEYVAULT") is string kv)
             builder.AddAzureKeyVault(new Uri($"https://{kv}.vault.azure.net/"), new DefaultAzureCredential());
