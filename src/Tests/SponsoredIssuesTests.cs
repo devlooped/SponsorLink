520264c69cb61d08ea200cabec8548ad8f1637f0
Daniel Cazzulino <daniel@cazzulino.com>
2024-08-30 13:56:07 -0300

    Make tests less dependent on configuration

--- src/Tests/SponsoredIssuesTests.cs
+++ src/Tests/SponsoredIssuesTests.cs
@@ -28,7 +28,7 @@ public class SponsoredIssuesTests : IDisposable
     [Fact]
     public async Task NewSponsorship()
     {
-        var sponsored = new SponsoredIssues(GetTable(), Services.GetRequiredService<SponsorLinkOptions>());
+        var sponsored = new SponsoredIssues(GetTable(), new SponsorLinkOptions { Account = "kzu" });
         await sponsored.AddSponsorship("kzu", "S_kwHOA6rues4AAwoC", 10);
         await sponsored.BackIssue("kzu", "S_kwHOA6rues4AAwoC", "owner/repo", 42, 1234);
 
@@ -39,7 +39,7 @@ public class SponsoredIssuesTests : IDisposable
     [Fact]
     public async Task AggregatesSponsorships()
     {
-        var sponsored = new SponsoredIssues(GetTable(), Services.GetRequiredService<SponsorLinkOptions>());
+        var sponsored = new SponsoredIssues(GetTable(), new SponsorLinkOptions { Account = "kzu" });
         await sponsored.AddSponsorship("kzu", "S_kwHOA6rues4AAwoC", 10);
         await sponsored.BackIssue("kzu", "S_kwHOA6rues4AAwoC", "owner/repo", 42, 1234);
 
@@ -53,7 +53,7 @@ public class SponsoredIssuesTests : IDisposable
     [Fact]
     public async Task EnumerateSponsorships()
     {
-        var sponsored = new SponsoredIssues(GetTable(), Services.GetRequiredService<SponsorLinkOptions>());
+        var sponsored = new SponsoredIssues(GetTable(), new SponsorLinkOptions { Account = "kzu" });
         await sponsored.AddSponsorship("kzu", "S_kwHOA6rues4AAwoC", 10);
         await sponsored.AddSponsorship("kzu", "S_kwHOA6rues4AAwoD", 20);
         await sponsored.AddSponsorship("kzu", "S_kwHOA6rues4AAwoE", 15);
@@ -67,7 +67,7 @@ public class SponsoredIssuesTests : IDisposable
     [Fact]
     public async Task AddsBackedAmountBadge()
     {
-        var sponsored = new SponsoredIssues(GetTable(), Services.GetRequiredService<SponsorLinkOptions>());
+        var sponsored = new SponsoredIssues(GetTable(), new SponsorLinkOptions { Account = "kzu" });
         await sponsored.AddSponsorship("kzu", "1", 10);
         await sponsored.BackIssue("kzu", "1","owner/repo", 42, 12345);
 
