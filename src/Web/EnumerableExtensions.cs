d9c6e4376597056344f5885cf2a25cbe2fd8c11e
Daniel Cazzulino <daniel@cazzulino.com>
2025-09-30 13:20:42 -0300

    Mention active sponsors in releases
    GitHub will auto-generate a Contributors section with the profile pics of all mentioned accounts.
    
    This will trigger the notification to all sponsors, which should be enough visibility.
    
    See for example https://github.com/devlooped/dotnet-retest/releases/tag/v1.0.0
    
    Also allow skipping the sponsors section entirely by adding `<!-- nosponsors -->` anywhere in the body.

---
+++ src/Web/EnumerableExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devlooped.Sponsors;
+
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// Batches the source sequence into chunks of the specified size.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source sequence to batch.</param>
+    /// <param name="size">The maximum size of each batch. Must be greater than 0.</param>
+    /// <returns>An IEnumerable of IEnumerables, each representing a batch of elements.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if source is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if size is less than or equal to 0.</exception>
+    public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, int size)
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+        if (size <= 0)
+            throw new ArgumentOutOfRangeException(nameof(size), "Size must be greater than 0.");
+
+        return BatchIterator(source, size);
+    }
+
+    static IEnumerable<IEnumerable<T>> BatchIterator<T>(IEnumerable<T> source, int size)
+    {
+        var batch = new List<T>(size);
+        foreach (var item in source)
+        {
+            batch.Add(item);
+            if (batch.Count == size)
+            {
+                yield return batch;
+                batch = new List<T>(size);
+            }
+        }
+        if (batch.Count > 0)
+        {
+            yield return batch;
+        }
+    }
+}
